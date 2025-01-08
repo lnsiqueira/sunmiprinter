f7dfa371df843b7a5b52d7653ca91d48312f3577
daniel Oliveira souza <sza.oliveiradaniel@gmail.com>
2025-01-08 18:01:00 -0300

    :sparkles: feat: add flutter project

---
+++ android/app/src/main/java/br/com/acbr/exemplo/sunmiprinter/SunmiPrinterFlutterPlugin.java
@@ -0,0 +1,149 @@
+package br.com.acbr.exemplo.sunmiprinter;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+import com.tectoy.sunmiprinter.SunmiPrinter;
+
+
+public class SunmiPrinterFlutterPlugin implements FlutterPlugin, MethodChannel.MethodCallHandler {
+
+    private Context context;
+    private SunmiPrinter sunmiPrinter;
+
+    private MethodChannel channel;
+    @Override
+    public void onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
+        context = binding.getApplicationContext();
+        sunmiPrinter = new SunmiPrinter(context);
+        channel = new MethodChannel(binding.getBinaryMessenger(), "br.com.acbr.exemplo.sunmiprinterplugin");
+        channel.setMethodCallHandler(this);
+    }
+
+    @Override
+    public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+
+    }
+
+    @Override
+    public void onMethodCall(@NonNull MethodCall call, @NonNull MethodChannel.Result result) {
+        switch (call.method) {
+            case "PrintTeste":
+                handlePrintTeste(result);
+                break;
+            case "getServiceVersion":
+                handleGetServiceVersion(result);
+                break;
+            case "printerInit":
+                handlePrinterInit(result);
+                break;
+            case "printerSelfChecking":
+                handlePrinterSelfChecking(result);
+                break;
+            case "getPrinterSerialNo":
+                handleGetPrinterSerialNo(result);
+                break;
+            case "getPrinterVersion":
+                handleGetPrinterVersion(result);
+                break;
+            case "getPrinterModal":
+                handleGetPrinterModal(result);
+                break;
+            case "getPrintedLength":
+                handleGetPrintedLength(result);
+                break;
+
+                case "cutPaper":
+                handleCutPaper(result);
+            default:
+                result.notImplemented();
+                break;
+        }
+    }
+
+    private void handleCutPaper(MethodChannel.Result result) {
+        try {
+            sunmiPrinter.cutPaper();
+            result.success(true);
+        } catch (Exception e) {
+            result.error("Erro ao cortar papel", e.getMessage(), e);
+        }
+    }
+
+    private void handlePrintTeste(@NonNull MethodChannel.Result result) {
+        try {
+            sunmiPrinter.PrintTeste();
+            result.success(true);
+        } catch (Exception e) {
+            result.error("Erro ao imprimir", e.getMessage(), e);
+        }
+    }
+
+    private void handleGetServiceVersion(@NonNull MethodChannel.Result result) {
+        try {
+            String version = sunmiPrinter.getServiceVersion();
+            result.success(version);
+        } catch (Exception e) {
+            result.error("Erro ao obter versão do serviço", e.getMessage(), e);
+        }
+    }
+
+    private void handlePrinterInit(@NonNull MethodChannel.Result result) {
+        try {
+            sunmiPrinter.printerInit();
+            result.success(true);
+        } catch (Exception e) {
+            result.error("Erro ao inicializar impressora", e.getMessage(), e);
+        }
+    }
+
+    private void handlePrinterSelfChecking(@NonNull MethodChannel.Result result) {
+        try {
+            sunmiPrinter.printerSelfChecking();
+            result.success(true);
+        } catch (Exception e) {
+            result.error("Erro ao verificar impressora", e.getMessage(), e);
+        }
+    }
+
+    private void handleGetPrinterSerialNo(@NonNull MethodChannel.Result result) {
+        try {
+            String serialNo = sunmiPrinter.getPrinterSerialNo();
+            result.success(serialNo);
+        } catch (Exception e) {
+            result.error("Erro ao obter serial da impressora", e.getMessage(), e);
+        }
+    }
+
+    private void handleGetPrinterVersion(@NonNull MethodChannel.Result result) {
+        try {
+            String version = sunmiPrinter.getPrinterVersion();
+            result.success(version);
+        } catch (Exception e) {
+            result.error("Erro ao obter versão da impressora", e.getMessage(), e);
+        }
+    }
+
+    private void handleGetPrinterModal(@NonNull MethodChannel.Result result) {
+        try {
+            String modal = sunmiPrinter.getPrinterModal();
+            result.success(modal);
+        } catch (Exception e) {
+            result.error("Erro ao obter modal da impressora", e.getMessage(), e);
+        }
+    }
+
+    private void handleGetPrintedLength(@NonNull MethodChannel.Result result) {
+        try {
+            sunmiPrinter.getPrintedLength();
+            result.success(true);
+        } catch (Exception e) {
+            result.error("Erro ao obter tamanho impresso", e.getMessage(), e);
+        }
+    }
+
+}
---
+++ lib/sunmiprinterplugin.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/services.dart';
+
+class SunmiPrinterPlugin {
+  static const MethodChannel _channel = MethodChannel('br.com.acbr.exemplo.sunmiprinterplugin');
+
+  Future<void> printTeste() async {
+    try {
+      await _channel.invokeMethod('PrintTeste');
+    } on PlatformException catch (e) {
+      throw 'Error printing test: ${e.message}';
+    }
+  }
+
+  Future<String> getServiceVersion() async {
+    try {
+      final String version = await _channel.invokeMethod('getServiceVersion');
+      return version;
+    } on PlatformException catch (e) {
+      throw 'Error getting service version: ${e.message}';
+    }
+  }
+
+  Future<void> printerInit() async {
+    try {
+      await _channel.invokeMethod('printerInit');
+    } on PlatformException catch (e) {
+      throw 'Error initializing printer: ${e.message}';
+    }
+  }
+
+  Future<void> printerSelfChecking() async {
+    try {
+      await _channel.invokeMethod('printerSelfChecking');
+    } on PlatformException catch (e) {
+      throw 'Error self-checking printer: ${e.message}';
+    }
+  }
+
+  Future<String> getPrinterSerialNo() async {
+    try {
+      final String serialNo = await _channel.invokeMethod('getPrinterSerialNo');
+      return serialNo;
+    } on PlatformException catch (e) {
+      throw 'Error getting printer serial number: ${e.message}';
+    }
+  }
+
+  Future<String> getPrinterVersion() async {
+    try {
+      final String version = await _channel.invokeMethod('getPrinterVersion');
+      return version;
+    } on PlatformException catch (e) {
+      throw 'Error getting printer version: ${e.message}';
+    }
+  }
+
+  Future<String> getPrinterModal() async {
+    try {
+      final String modal = await _channel.invokeMethod('getPrinterModal');
+      return modal;
+    } on PlatformException catch (e) {
+      throw 'Error getting printer modal: ${e.message}';
+    }
+  }
+
+  Future<void> getPrintedLength() async {
+    try {
+      await _channel.invokeMethod('getPrintedLength');
+    } on PlatformException catch (e) {
+      throw 'Error getting printed length: ${e.message}';
+    }
+  }
+}
