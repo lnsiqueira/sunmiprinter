3bb32df68846c3f13be16c4ef6af4b2a57d6ca1e
daniel Oliveira souza <sza.oliveiradaniel@gmail.com>
2025-01-16 10:46:45 -0300

    :lipstick: feat: change app color scheme to blue

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Demo SunmiPrinter',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Demo SunmiPrinter Home Page'),
