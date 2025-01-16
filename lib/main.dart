7ad57ccc851990b9bc6f745253a65b67a3efe5b2
daniel Oliveira souza <sza.oliveiradaniel@gmail.com>
2025-01-16 14:21:33 -0300

    :broom: cleanup: remove unnecessary imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
-import 'package:demosunmiprinter/sunmiprinterplugin.dart';
-import 'package:flutter/material.dart';
 import 'package:demosunmiprinter/screens/home_screen.dart';
+import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
