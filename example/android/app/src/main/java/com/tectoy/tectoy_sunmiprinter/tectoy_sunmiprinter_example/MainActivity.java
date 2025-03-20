a3f93c841919e25c5fb4351d289f4e4905bc9f29
doszapro <daniel.oliveira@projetoacbr.com.br>
2025-03-20 16:43:08 -0300

    :recycle: refactor: ajusts project to support

---
+++ example/android/app/src/main/java/com/tectoy/tectoy_sunmiprinter/tectoy_sunmiprinter_example/MainActivity.java
@@ -0,0 +1,6 @@
+package com.tectoy.tectoy_sunmiprinter.tectoy_sunmiprinter_example;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
--- tectoy_sunmiprinter/android/libs/README.md
+++
@@ -1,6 +0,0 @@
-
-README
----
-
-1 - Baixe a biblioteca [tectoy.jar](https://github.com/TecToy-Automacao/Java/raw/refs/heads/master/jar/Tectoy/tectoy.jar)
-2 - Adicione a biblioteca nesta pasta.
